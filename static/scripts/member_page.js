950ddeb8b6baf3a9c976593db33e8c90b18640d7
mayonnaise0403 <rita09436@gmail.com>
2023-02-25 18:36:02 +0800

    fix: 解決非同步問題

--- static/scripts/member_page.js
+++ static/scripts/member_page.js
@@ -527,7 +527,10 @@ friendPopupClose.addEventListener("click", () => {
 chatCloseBtn.addEventListener("click", () => {
     stickerPopup.style.visibility = "hidden";
     chatContainer.style.display = "none";
-    chatMsgDate.style.visibility = "hidden";
+    if (chatMsgDate) {
+        chatMsgDate.style.visibility = "hidden";
+    }
+
     const parentDiv = document.querySelector(".chat-message");
     while (parentDiv.firstChild) {
         parentDiv.removeChild(parentDiv.firstChild);
@@ -634,6 +637,47 @@ messageInput.addEventListener("compositionend", (e) => {
 })
 
 
+
+
+function createGroupList(groupData, list) {
+    let count = 0;
+    list.innerHTML = "";
+    if (groupData.group_list.length === 0) {
+        groupListEmpty.style.display = "block";
+        groupListEmpty.style.display = "flex";
+    } else {
+        groupListEmpty.style.display = "none";
+    }
+
+    groupData.group_list.forEach(element => {
+        newDiv = document.createElement("div");
+        newDiv.className = "group";
+        list.appendChild(newDiv);
+        let group = document.querySelectorAll(".group");
+
+        newImg = document.createElement("img");
+        newImg.src = element.headshot;
+        newImg.id = "group-headshot";
+        group[count].appendChild(newImg);
+
+        newP = document.createElement("p");
+        newP.innerHTML = element.group_name;
+        group[count].appendChild(newP);
+
+        group[count].addEventListener("click", () => {
+            let friendPopupHeadshot = document.querySelector(".friend-popup-headshot");
+            // friendPopupHeadshot.src = "./images/Loading_icon.gif";
+            // friendPopupHeadshot.onload = () => {
+            //     friendPopupHeadshot.src = element.headshot;
+            // }
+            friendPopupHeadshot.src = element.headshot;
+            friendPopup.style.display = "block";
+            friendName.innerHTML = element.group_name;
+            friendId = element.group_id;
+        })
+        count++;
+    })
+}
 
 
 
