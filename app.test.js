5ee8ba223380b87c9e2a5593b641032876e1d44b
mayonnaise0403 <rita09436@gmail.com>
2023-03-14 14:26:29 +0800

    新增homepage input value

--- app.test.js
+++ app.test.js
@@ -50,23 +50,21 @@ app.use(updateRouter);
 
 
 describe('PUT /group_name', () => {
-    it('should respond with status 200 and new group name if update succeeds', async () => {
-        const body = { groupId: '123', groupName: 'newGroupName' }
+    it('should return 200 status code and new group name when update group name successfully', async () => {
+        const groupId = 'test-group-id';
+        const groupName = 'test-group-name';
+        const updateGroupNameStub = sinon.stub(Update, 'updateGroupName').withArgs(groupId, groupName).resolves(true);
 
-        // 建立一個 updateGroupName 的 stub，讓它返回預期的值 true
-        const updateGroupNameStub = sinon.stub(Update, 'updateGroupName').returns(true);
-        // 呼叫 API
         const res = await request(app)
             .put('/group_name')
-            .send(body);
+            .send({ groupId, groupName });
 
-        // 驗證回應是否正確
-        expect(res.status).to.equal(500);
-        expect(res.body).to.equal({ message: "伺服器內部發生錯誤", status: "error" });
+        sinon.assert.calledOnce(updateGroupNameStub);
+        sinon.assert.calledWith(updateGroupNameStub, groupId, groupName);
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal('success');
+        expect(res.body.newGroupName).to.equal(groupName);
 
-
-
-        // 還原 stub 的行為
         updateGroupNameStub.restore();
     });
 
